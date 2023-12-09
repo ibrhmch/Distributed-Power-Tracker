73a574ccea6d391e1e745ed0ee4a0d363ef9b239
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-12-09 15:37:21 -0700

    updated only create the database if it is not already there

--- psql/init.sql
+++ psql/init.sql
@@ -1,8 +1,14 @@
-CREATE DATABASE solardata;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'solardata') THEN
+        CREATE DATABASE solardata;
+    END IF;
+END
+$$;
 
-\c solardata;
+\connect solardata;
 
-CREATE TABLE solar_panels_data (
+CREATE TABLE IF NOT EXISTS solar_panels_data (
     panel_id UUID PRIMARY KEY,
     recorded_at TIMESTAMP NOT NULL,
     power_kwh FLOAT NOT NULL,
