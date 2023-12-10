0eae7f8d50a108d0dcc3cd4bd84e3956fca82b27
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-12-10 01:27:56 -0700

    socketServer to serve live data to the front end

--- rest/socketServer.py
+++ rest/socketServer.py
@@ -2,25 +2,42 @@ import random
 import threading
 import time
 
+import psycopg2
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
-CORS(
-    app
-)  # Enable CORS for all domains. For specific domains, use CORS(app, origins=["http://localhost:3000"])
-socketio = SocketIO(
-    app, cors_allowed_origins="http://localhost:3000"
-)  # Allow your React app's origin
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")
+
+# Database connection parameters - update these with your database details
+DB_HOST = "localhost"
+DB_NAME = "solardata"
+DB_USER = "postgres"
+DB_PASS = "12345678"
+DB_PORT = "5433"
+
+
+def get_random_kwh_value():
+    conn = psycopg2.connect(
+        dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT power_kwh FROM solar_panels_data ORDER BY RANDOM() LIMIT 1")
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return result[0] if result else None
 
 
 def send_random_number():
     while True:
         time.sleep(5)
-        random_number = random.randint(1, 100)
-        socketio.emit("new_number", {"number": random_number})
+        random_kwh = get_random_kwh_value()
+        if random_kwh is not None:
+            socketio.emit("new_number", {"number": random_kwh})
 
 
 @app.route("/")
