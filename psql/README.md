9e7110e32fbc02f2702c84acb98b0c3bacefe21b
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-12-10 19:48:24 -0700

    updated init.sql for new database schema

--- psql/README.md
+++ psql/README.md
@@ -3,14 +3,55 @@
 The Postgres database is called `solardata` and it has table called `solar_panels_data` with the following schema:
 
 ```sql
-CREATE TABLE solar_panels_data (
-    panel_id UUID PRIMARY KEY,
-    recorded_at TIMESTAMP NOT NULL,
-    power_kwh FLOAT NOT NULL,
-    latitude FLOAT,
-    longitude FLOAT
+CREATE TABLE solar_panels (
+    panel_id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL
 );
 ```
+
+```sql
+CREATE TABLE solar_panels_data (
+    data_id SERIAL PRIMARY KEY,
+    panel_id VARCHAR(255) NOT NULL,
+    recorded_at TIMESTAMP NOT NULL,
+    power_kwh FLOAT NOT NULL,
+    FOREIGN KEY (panel_id) REFERENCES solar_panels(panel_id)
+);
+```
+
+
+## To create new solar panels, run:
+```sql
+INSERT INTO solar_panels (panel_id, name, latitude, longitude)
+VALUES 
+('EC1', 'Engineering Center', 140.007259752621174, -105.26350055410776),
+('C4C1', 'Center For Community', 140.00389502140387, -105.26616659936727);
+```
+
+## To create new solar panel data, run:
+```sql
+DO $$
+DECLARE
+    i INT;
+    time_offset INTERVAL;
+BEGIN
+    FOR i IN 0..9 LOOP
+        time_offset := (i * INTERVAL '5 minutes');
+
+        -- Insert data for 'Engineering Center'
+        INSERT INTO solar_panels_data (panel_id, recorded_at, power_kwh)
+        VALUES ('EC1', NOW() - time_offset, ROUND((RANDOM() * 100)::numeric, 2));
+
+        -- Insert data for 'Center For Community'
+        INSERT INTO solar_panels_data (panel_id, recorded_at, power_kwh)
+        VALUES ('C4C1', NOW() - time_offset, ROUND((RANDOM() * 100)::numeric, 2));
+    END LOOP;
+END
+$$;
+```
+
 
 ## To create the deployment, run:
 ```bash
--- psql/init.sql
+++ psql/init.sql
@@ -1,17 +1,51 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'solardata') THEN
-        CREATE DATABASE solardata;
-    END IF;
-END
-$$;
+-- Drop the 'solardata' database if it exists
+DROP DATABASE IF EXISTS solardata;
 
+-- Create the 'solardata' database
+CREATE DATABASE solardata;
+
+-- Connect to the 'solardata' database
 \connect solardata;
 
-CREATE TABLE IF NOT EXISTS solar_panels_data (
-    panel_id UUID PRIMARY KEY,
-    recorded_at TIMESTAMP NOT NULL,
-    power_kwh FLOAT NOT NULL,
-    latitude FLOAT,
-    longitude FLOAT
+-- Create 'solar_panels' table
+CREATE TABLE solar_panels (
+    panel_id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL
 );
+
+-- Create 'solar_panels_data' table
+CREATE TABLE solar_panels_data (
+    data_id SERIAL PRIMARY KEY,
+    panel_id VARCHAR(255) NOT NULL,
+    recorded_at TIMESTAMP NOT NULL,
+    power_kw FLOAT NOT NULL,
+    FOREIGN KEY (panel_id) REFERENCES solar_panels(panel_id)
+);
+
+-- Insert initial data into 'solar_panels'
+INSERT INTO solar_panels (panel_id, name, latitude, longitude)
+VALUES 
+('EC1', 'Engineering Center', 140.007259752621174, -105.26350055410776),
+('C4C1', 'Center For Community', 140.00389502140387, -105.26616659936727);
+
+-- Insert initial data into 'solar_panels_data'
+DO $$
+DECLARE
+    i INT;
+    time_offset INTERVAL;
+BEGIN
+    FOR i IN 0..9 LOOP
+        time_offset := (i * INTERVAL '5 minutes');
+
+        -- Insert data for 'Engineering Center'
+        INSERT INTO solar_panels_data (panel_id, recorded_at, power_kw)
+        VALUES ('EC1', NOW() - time_offset, ROUND((RANDOM() * 100)::numeric, 2));
+
+        -- Insert data for 'Center For Community'
+        INSERT INTO solar_panels_data (panel_id, recorded_at, power_kw)
+        VALUES ('C4C1', NOW() - time_offset, ROUND((RANDOM() * 100)::numeric, 2));
+    END LOOP;
+END
+$$;
