771f362f32442b3f5105d83082e263ba41b85407
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-12-13 16:10:29 -0700

    updated deploy-sh to set up the project

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,20 +1,24 @@
 kubectl apply -f redis/redis-deployment.yaml
 kubectl apply -f redis/redis-service.yaml
-kubectl apply -f minio/minio-external-service.yaml
-helm install -f minio/minio-config.yaml -n minio-ns --create-namespace minio-proj bitnami/minio
-# kubectl apply -f minio/minio-dep.yaml
-kubectl apply -f minio/minio-external-service.yaml
+kubectl apply -f psql/psql-deployment.yaml
+kubectl apply -f psql/psql-service.yaml
 
-kubectl apply -f js_frontend/frontend-deployment.yaml
-kubectl apply -f js_frontend/frontend-service.yaml
+sleep 5
+kubectl apply -f rest-server/rest-deployment.yaml
+kubectl apply -f rest-server/rest-service.yaml
+kubectl apply -f rest-server/rest-ingress.yaml
+
+sleep 5
+kubectl apply -f socket/ss-deployment.yaml
+kubectl apply -f socket/ss-service.yaml
+# kubectl apply -f socket/ss-ingress.yaml
 
 sleep 10
-kubectl port-forward --address 0.0.0.0 service/redis 6379:6379 &
-kubectl port-forward --address 0.0.0.0 service/site 3000:3000 &
+kubectl apply -f frontend/front-deployment.yaml
+kubectl apply -f frontend/front-service.yaml
+kubectl apply -f frontend/front-ingress.yaml
 
-# If you're using minio from the kubernetes tutorial this will forward those
-kubectl port-forward -n minio-ns --address 0.0.0.0 service/minio-proj 9000:9000 &
-kubectl port-forward -n minio-ns --address 0.0.0.0 service/minio-proj 9001:9001 &
+sleep 5
 
-# while true; do kubectl port-forward --namespace minio-ns svc/minio-proj 9001:9001&; done
+# while true; do kubectl port-forward svc/socket 5001:5001&; done
 
